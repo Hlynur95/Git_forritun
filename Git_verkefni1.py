e668d53c237c297462ac4a102c5427f2f85cc380
hlynur95 <hlynurhy283@nemi.tskoli.is>
2017-01-27 09:38:27 +0000

    commenta fyrir lið 1

--- Git_verkefni1.py
+++ Git_verkefni1.py
@@ -4,21 +4,21 @@
 
 svar = "ja"
 
-while svar == "ja":
+while svar == "ja": #while likkja yfir öll dæmin
 
     print("veldur 1,2,3 fyrir dæmi. 4 til að hætta")
-    val = int(input("sláðu in 1-4 fyrir hvað þú vilt gera: "))
+    val = int(input("sláðu in 1-4 fyrir hvað þú vilt gera: ")) #læta notandan velja hvað hann vill gera
 
     if val == 1:
-        summa1 = 0
-        summa2 = 1
+        summa1 = 0 #gildi til að finna samantals talnana
+        summa2 = 1 #gildi til að finna margföldun talnana
 
-        for x in range(2):
-            tala = int(input("sláðu inn tölu: "))
-            summa1 = summa1 + tala
-            summa2 = summa2 * tala
+        for x in range(2): #for lykkja sem bíður um 2 stafi
+            tala = int(input("sláðu inn tölu: ")) #skrifar in stafina
+            summa1 = summa1 + tala # leggja saman stafina
+            summa2 = summa2 * tala # margfalda stafina saman
 
-        print("saman lagt",summa1)
+        print("saman lagt",summa1) 
         print("margfaldað",summa2)
 
 
@@ -40,9 +40,9 @@ while svar == "ja":
         for x in range(len(texti)):
             if texti [x].isalpha() and texti[x].isupper():
                 storstafur = storstafur + 1
-                if (texti[x+1].islower()):
+                if texti[x+1].islower():
                     eftirstafur = eftirstafur + 1
-            if (texti[x].isalpha() and texti[x].islower()):
+            if texti[x].isalpha() and texti[x].islower():
                 litilstafur = litilstafur + 1
 
         print("það eru",storstafur,"storir stafir",litilstafur,"litlir stafir og",eftirstafur,"lágstafir a eftir hástafi")
