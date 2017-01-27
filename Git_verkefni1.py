f43b4d4f832006acf0e5f66d5bad80313909bac7
hlynur95 <hlynurhy283@nemi.tskoli.is>
2017-01-27 09:10:04 +0000

    klára fyrsta lið

--- Git_verkefni1.py
+++ Git_verkefni1.py
@@ -7,26 +7,37 @@ svar = "ja"
 while svar == "ja":
 
     print("veldur 1,2,3 fyrir dæmi. 4 til að hætta")
-    val = int(input("sláðu in 1-4 fyrir hvað þú vilt gera"))
+    val = int(input("sláðu in 1-4 fyrir hvað þú vilt gera: "))
 
     if val == 1:
+        summa1 = 0
+        summa2 = 1
+
+        for x in range(2):
+            tala = int(input("sláðu inn tölu: "))
+            summa1 = summa1 + tala
+            summa2 = summa2 * tala
+
+        print("saman lagt",summa1)
+        print("margfaldað",summa2)
 
 
 
 
 
-    if val == 2:
+
+    #if val == 2:
 
 
 
 
 
-    if val == 3:
+    #if val == 3:
 
 
 
 
-    if val == 4:
+    #if val == 4:
 
 
 
