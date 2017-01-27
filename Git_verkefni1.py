e6c028dc782786a217e7c60ac12aea9451dcccb2
hlynur95 <hlynurhy283@nemi.tskoli.is>
2017-01-27 09:02:05 +0000

    gera while lykkju yfyr dæmið til að einfalda að keyra það

--- Git_verkefni1.py
+++ Git_verkefni1.py
@@ -1,3 +1,34 @@
 #hlynur Smári Gyðuson
 #Git Verkefni
 #27.1.2017
+
+svar = "ja"
+
+while svar == "ja":
+
+    print("veldur 1,2,3 fyrir dæmi. 4 til að hætta")
+    val = int(input("sláðu in 1-4 fyrir hvað þú vilt gera"))
+
+    if val == 1:
+
+
+
+
+
+    if val == 2:
+
+
+
+
+
+    if val == 3:
+
+
+
+
+    if val == 4:
+
+
+
+
+
